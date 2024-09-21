575ca330b40a05a7ac60f8aa2dd2e9ed92790888
Abhishek-Biswas-Deep <109815149+Abhishek-Biswas-Deep@users.noreply.github.com>
2024-09-20 22:33:10 -0300

    Update BullyBot.java

--- src/BullyBot.java
+++ src/BullyBot.java
@@ -1,9 +1,5 @@
 /**
    File: BullyBot.java
-   Author: Alex Brodsky
-   Date: September 21, 2015
-   Purpose: CSCI 1110, Assignment 4 Solution
-
    Description: This class specifies the BullyBot class.
 */
 
