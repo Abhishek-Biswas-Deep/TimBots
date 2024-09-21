51774a8173400ff2376d3087cc19bce82567e883
Abhishek-Biswas-Deep <109815149+Abhishek-Biswas-Deep@users.noreply.github.com>
2024-09-20 22:33:37 -0300

    Update SpressoBot.java

--- src/SpressoBot.java
+++ src/SpressoBot.java
@@ -1,9 +1,5 @@
 /**
    File: SpressoBot.java
-   Author: Alex Brodsky
-   Date: September 21, 2015
-   Purpose: CSCI 1110, Assignment 4 Solution
-
    Description: This class specifies the SpressoBot class.
 */
 
